8a661a327dff34bfd22d4b967f5543560bd597e9
Pau <p41stg@proton.me>
2024-12-14 10:30:51 +0100

    Creacion del objeto tipo perfil Declarados 3 hobbies,3 foods y 3 funFacts Ahora se muestra toda la información del perfil por consola

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,25 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //Añadir listas
+        ArrayList<String> hobbies = new ArrayList<>();
+        hobbies.add("\uD83C\uDFB5");
+        hobbies.add("\uD83C\uDFC2");
+        hobbies.add("\uD83E\uDD3F");
+
+        ArrayList<String> foods = new ArrayList<>();
+        foods.add("\uD83C\uDF55");
+        foods.add("\uD83C\uDF56");
+        foods.add("\uD83C\uDF66");
+
+        ArrayList<String> funFacts = new ArrayList<>();
+        funFacts.add("Escucho musica a todas horas!");
+        funFacts.add("Podria pasarme el dia haciendo deporte");
+        funFacts.add("Duermo menos de lo que deberia \uD83D\uDE02");
+
+        //Crear el objeto perfil y mostrar la información
+        Perfil perfil = new Perfil("Pau", "Soy una persona muy casera", hobbies, foods, funFacts);
+        perfil.mostrarPerfil();
 
     }
 }
